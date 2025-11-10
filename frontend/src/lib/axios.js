06eef4bfb0fe43a9b34ae07a9df8845e25c45507
Tushar <tkumar2808@gmail.com>
2025-11-11 01:43:12 +0530

    fixed deployement

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api" // local backend
+    : "https://chatly-chat-app-7epe.onrender.com/api"; // Render backend
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  withCredentials: true,
+  baseURL: API_BASE_URL,
+  withCredentials: true, // important for cookie-based auth
 });
